d0d3b98ae3a104bea850b84af321930c13e880b4
peraa0837 <peraa0837@HRH-IC0027324.wrdsb.ca>
2017-03-29 10:04:30 -0400

    final commit for hangman

--- src/Hangman.java
+++ src/Hangman.java
@@ -18,40 +18,73 @@ public class Hangman {
         // TODO code application logic here
         //new scanner
         Scanner input = new Scanner(System.in);
+
+
         while (true) {
             //prompt first player to enter a word
             System.out.println("Player 1: Enter the word you wish player 2 to guess.");
             String word = input.nextLine();
+            //converts word to lower case for consistency
+            word = word.toLowerCase();
             //gets the length of the entered word
             int length = word.length();
             //prints out blanks for every letter
+            String dashes = "";
             for (int blanks = 0; blanks < length; blanks = blanks + 1) {
-                System.out.print("_ ");
+                dashes = dashes + "-";
             }
-            //creates lives for the second player and enters a loop where lives are required
-            int lives = 0;
-            while (lives < 6) {
-                //player 2 guesses a letter and the program checks if it is correct or not
-                System.out.println("Player 2: Guess a letter.");
-                String guess = input.nextLine();
-                for (int i = 0; i < length;) {
-                    char guessChar = word.charAt(i);
-                    //prints out the character in the correct position if it is a correct guess
-                    if (guess.equals("" + guessChar)) {
-                        System.out.println("Correct");
-                        int position = word.indexOf(guess);
-                        
-                    } else {
-                        System.out.println("You lost one life.");
-                        lives++;
-                        break;
-                    }
-                    i++;
-                }
+            //creates a stringbuilder in order to setCharAt
+            StringBuilder temp = new StringBuilder(dashes);
 
+
+            //creates integers for lives and correct guesses
+            int correct = 0;
+            int lives = 6;
+            while (lives > 0) {
+                //prompts user to guess a letter
+                System.out.println(temp);
+                System.out.println("Player 2: Guess a letter. You have " + lives + " lives left.");
+                String guess = input.nextLine();
+                //converts guess to lower case to maintain consistency
+                guess = guess.toLowerCase();
+                //turns the guess string into a character
+                char guessChar = guess.charAt(0);
+                //creates an integer for the position of the letter in the word
+                int x = word.indexOf(guessChar);
+
+                //if correct, search for more of the same character then replace dashes
+                if (x >= 0) {
+                    System.out.println("Correct.");
+                    while (x >= 0) {
+                        temp.setCharAt(x, guessChar);
+                        x = word.indexOf(guessChar, x + 1);
+                        if (x >= 0) {
+                            correct++;
+                        }
+                    }
+                    correct++;
+                    //if incorrect, lose 1 life
+                } else {
+                    System.out.println("You've lost one life.");
+                    lives--;
+                }
+                //if word is succesfully completed, say winning string
+                if (correct == length) {
+                    System.out.println("Congratulations, you won!");
+                    break;
+                }
             }
-            System.out.println("You've lost all your lives. Game over.");
-            break;
+            //if all lives are lost, end game
+            if (correct < length) {
+                System.out.println("You've lost all your lives. Game over.");
+            }
+            //ask to play again
+            System.out.println("Play again? (Yes/No)");
+            String restart = input.nextLine();
+            restart = restart.toLowerCase();
+            if (restart.equals("no")) {
+                break;
+            }
         }
     }
 }
